91fab225d290dbab3af1d60ae48d85a8c6ee1e90
abdulganiyy <lasisibalogunopeoluwa@ymail.com>
2021-11-07 16:55:42 +0100

    added all files

--- src/components/Menubar/Menubar.js
+++ src/components/Menubar/Menubar.js
@@ -3,12 +3,16 @@ import { FaListUl, FaSearch } from "react-icons/fa";
 
 import { Link } from "react-router-dom";
 
+import Logo from "../../assets/sls_logo.png";
+
 import "./Menubar.css";
 
 const Menubar = () => {
   return (
     <div className="menubar">
-      <div className="logo-box">SLS</div>
+      <div className="logo-box">
+        <img src={Logo} alt="logo" />
+      </div>
       <div className="navigation-items">
         <ul>
           <li>
