37fd304bba711327c67470dfa21a581dde021dd4
abdulganiyy <lasisibalogunopeoluwa@ymail.com>
2022-01-12 16:47:45 +0100

    Added application page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer/Footer";
 
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Apply from "./pages/Apply/Apply";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="apply-for-free-coding-events-workshops" element={<Apply />} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/pages/Apply/Apply.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Apply.css'
+
+const Apply = () => {
+    return (
+        <div className='application'>
+            <a href='https://docs.google.com/forms/d/1ciJeHtiXrwqhKSMbBCMHSUDrKn_sE_0AEd3N9OJQ240/prefill' className='application-link'>Apply for the free coding events and workshops</a>
+        </div>
+    )
+}
+
+export default Apply
